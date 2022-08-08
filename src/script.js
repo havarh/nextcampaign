bc83fff61e80f8f3d78c76e0745b052a13e75f12
havarh <github@havar.no>
2022-08-08 12:49:41 +0200

    More history, newer old version

--- src/script.js
+++ src/script.js
@@ -9,8 +9,9 @@
 //serverTime = new Date('Sept 4 2022 16:59:50 UTC+1');
 //serverTime = new Date('Feb 20 2022 23:59:50 UTC+1');
 //serverTime = new Date('Dec 31 2021 23:59:50 UTC+1');
-var localTime = Date.now();
-var timeDiff = 0;
+var localTime = Date.now(),
+    timeDiff = 0,
+    showSeconds = true;
 
 if (typeof serverTime != "undefined")
   timeDiff = serverTime - localTime;
@@ -19,14 +20,28 @@ function today() {
 }
 const DateTime = luxon.DateTime;
 const Duration = luxon.Duration;
-
+// rounds down Date object or timestamp in milliseconds to nearest minute
+function roundToNearestMinute(date) {
+  const minutes = 1;
+  const ms = 1000 * 60 * minutes;
+  if (typeof date == "number")
+    return (Math.floor(date / ms) * ms);
+  return new Date(Math.floor(date.getTime() / ms) * ms);
+}
 function getTimeRemaining(endtime) {
-  var diff = DateTime.fromMillis(new Date(endtime).getTime()).diff(DateTime.fromMillis(today().getTime()), ['months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds']);
+  var endtimeMilli = new Date(endtime).getTime(),
+      todayMilli = today().getTime(),
+      diff = DateTime.fromMillis(endtimeMilli).diff(DateTime.fromMillis(todayMilli), ['months', 'days', 'hours', 'minutes', 'seconds']);
+  // if 1 or more months or more than 7 days left, round down to nearest minute since seconds aren't shown
+  if (diff.months > 0 || diff.days > 7) {
+    showSeconds = false;
+    diff = DateTime.fromMillis(endtimeMilli).diff(DateTime.fromMillis(roundToNearestMinute(todayMilli)), ['months', 'days', 'hours', 'minutes', 'seconds']);
+  }
   var months = diff.months,
       days = diff.days,
       hours = diff.hours,
       minutes = diff.minutes,
-      seconds = diff.seconds,
+      seconds = Math.floor(diff.seconds),
       finish = months == 0 &&
                days == 0 &&
                hours == 0 &&
@@ -52,7 +67,7 @@ function initializeClock(id, endtime) {
   //const monthsSpan = clock.querySelector('.months');
 
   function updateClock() {
-    const timer = getTimeRemaining(endtime);
+    var timer = getTimeRemaining(endtime);
     monthsSpan.innerHTML = timer.months;
     daysSpan.innerHTML = timer.days;
     // hide months and/or days if they're 0
@@ -61,10 +76,10 @@ function initializeClock(id, endtime) {
     
     hoursSpan.innerHTML = ('0' + timer.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + timer.minutes).slice(-2);
-    // no need to show seconds when there's more than 1 month to the next campaign
-    if (timer.months < 1)
+    // no need to show seconds when there's more than 1 month or more than 7 days to the next campaign
+    if (showSeconds) // timer.months < 1 || timer.days <= 7
       secondsSpan.innerHTML = ('0' + timer.seconds).slice(-2);
-    secondsSpan.parentElement.classList.toggle("hidden", timer.months > 0);
+    secondsSpan.parentElement.classList.toggle("hidden", !showSeconds); //timer.months > 0 || timer.days > 7
     if (timer.finish) {
       console.log("finish");
       //clearInterval(timeinterval);
@@ -84,7 +99,6 @@ function initializeClock(id, endtime) {
       updateClock();
     }, (1100) - ms );    
   }
-
   updateClock();
   //const timeinterval = setInterval(updateClock, 1000);
 }
@@ -118,7 +132,7 @@ function ready() {
       document.getElementById("campaignName").innerHTML = campaignName;
       document.getElementById("currentCampaignName").innerHTML = currentCampaignName;
       document.getElementById("releasedate").innerHTML =
-        DateTime.fromMillis(new Date(endDate).getTime()).setLocale('en-UK').toLocaleString(luxon.DateTime.DATETIME_FULL);
+        DateTime.fromMillis(new Date(endDate).getTime()).setLocale('en-UK').toLocaleString(DateTime.DATETIME_FULL);
       initializeClock('clockdiv', endDate);
     }
   });
