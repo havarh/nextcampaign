d616f193fc656fd910f4b05105c567a861b5bb44
havarh <github@havar.no>
2022-08-08 13:33:09 +0200

    Cleanup of old code

--- src/time.php
+++ src/time.php
@@ -5,7 +5,5 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header("Pragma: no-cache"); // HTTP/1.0
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-//echo time()."\n";
-//echo microtime();
 ?>
 var serverTime = <?php echo floor(microtime(true) * 1000); ?>;
